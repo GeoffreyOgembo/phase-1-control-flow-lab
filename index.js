c7de75fa07c29468fd93b9b4a4aaae914620d848
GeoffreyOgembo <geoffereyogembo06@gmail.com>
2022-06-09 03:03:32 -0400

    control slow

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 function scuberGreetingForFeet(){
-  // Write your code here!
-}
+  function scuberGreetingForFeet(feet){
+    // Write your code here!
+    if (feet <= 400){
+      return 'This one is on me!'
+    }
+    else if (feet>2500){
+      return 'No can do.'
+    }
+  else if (feet >2000){
+    return 'I will gladly take your thirty bucks.'
+  }
+  }
+  function ternaryCheckCity(){
+    function ternaryCheckCity(){
+      function ternaryCheckCity(city){
+        
+        {return city === 'NYC' ?  "Ok, sounds good.": 'No go.'}
+      
+        function switchOnCharmFromTip(){
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+        } 
+      }
