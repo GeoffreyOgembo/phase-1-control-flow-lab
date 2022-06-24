75f253780717afac998280e0d9ed470c1e467998
geoffreyogembo <geoffreyogembo06@gmail.com>
2022-06-24 06:37:54 -0400

    "resubmitted"

--- index.js
+++ index.js
@@ -22,8 +22,7 @@ function switchOnCharmFromTip(tip){
   console.log(tip);
   if (tip === 'generous') {
     return 'Thank you so much.'
-  } 
-  else if (tip === 'not as generous') {
+  } else if (tip === 'not as generous') {
     return 'Thank you.';
   } else {
     return 'Bye.';
